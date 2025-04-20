2d5985866132be6e185528849f7829bbce795a77
Michael Black <wmblack23@gmail.com>
2025-04-19 23:24:50 -0600

    Updated API version to 4o

--- utils.py
+++ utils.py
@@ -10,7 +10,7 @@ client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 def correct_spanish_entry(text):
     response = client.chat.completions.create(
-        model="gpt-4-turbo",
+        model="gpt-4",
         messages=[
             {
                 "role": "system",
